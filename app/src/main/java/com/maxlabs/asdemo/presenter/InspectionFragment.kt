323633b493d3fed31b69bcd3faa281cf5629482b
Nidhi Srivastava <pixylabs77@gmail.com>
2024-07-16 14:17:48 +0530

    Adding  code to github

--- app/src/main/java/com/maxlabs/asdemo/presenter/InspectionFragment.kt
+++ app/src/main/java/com/maxlabs/asdemo/presenter/InspectionFragment.kt
@@ -35,7 +35,7 @@ class InspectionFragment : Fragment() {
     //private var totalScore: Float = 0f
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // TODO: Use the ViewModel
+
     }
 
     override fun onCreateView(
